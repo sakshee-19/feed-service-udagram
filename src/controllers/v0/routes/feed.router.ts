983eb06d8dff93a8c3651428a496a1f7985baec5
saksjain <saksjain@adobe.com>
2020-04-17 15:11:32 +0530

    add method to post feed

--- src/controllers/v0/routes/feed.router.ts
+++ src/controllers/v0/routes/feed.router.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from "express";
 import { FeedItem } from "../models/Feed";
-import { getGetSignedUrl } from "../../../aws";
+import { getGetSignedUrl, getPutSignedUrl } from "../../../aws";
+import { NextFunction } from 'connect';
+import * as jwt from 'jsonwebtoken';
+import { config } from "../../../config/config";
+
 
 const router : Router = Router();
 
@@ -21,6 +25,47 @@ router.get('/:id', async(req:Request, res:Response) => {
         res.status(200).send(item);
     }
     res.status(404).send(item);
+
+});
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+    //  return next();
+       if (!req.headers || !req.headers.authorization){
+           return res.status(401).send({ message: 'No authorization headers.' });
+       }
+       
+   
+       const token_bearer = req.headers.authorization.split(' ');
+       if(token_bearer.length != 2){
+           return res.status(401).send({ message: 'Malformed token.' });
+       }
+       
+       const token = token_bearer[1];
+       return jwt.verify(token, config.jwt.secret , (err, decoded) => {
+         if (err) {
+           return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+         }
+         return next();
+       });
+   }
+
+router.post('/', async(req: Request, res:Response) => {
+    const {url, caption} = req.body;
+    if(!url || !caption){
+        res.status(400).send("caption and url required");
+    }
+    const putUrl = getPutSignedUrl(url);
+    const feed = new FeedItem();
+    feed.caption = caption;
+    feed.url = url;
+    var savedFeed : FeedItem;
+    try{
+        savedFeed = await feed.save();
+    }catch(err) {
+        res.status(400).send(err.message);
+    }
+    savedFeed.url = putUrl;
+    res.status(201).send(savedFeed);
 
 });
 
