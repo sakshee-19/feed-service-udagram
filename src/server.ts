8e6b6e44148c628e380256183b567c608968bb6b
saksjain <saksjain@adobe.com>
2020-04-20 23:51:36 +0530

    add docker file

--- src/server.ts
+++ src/server.ts
@@ -16,13 +16,12 @@ import { V0MODEL } from "./controllers/v0/models";
 
     const port = process.env.PORT || 8001; // default port to listen
 
-    //CORS Should be restricted
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        next();
-    });
-
+ //CORS Should be restricted
+ app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
     app.use('/api/v0/', IndexRouter);
 
     app.get('/', (req, res) => {
